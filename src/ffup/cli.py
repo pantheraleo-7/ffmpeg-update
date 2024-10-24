dde333fe5fdb68e73d999f840950addf7579bcb8
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-10-24 11:11:27 +0530

    use tempfile.TemporaryDirectory; refactor

--- src/ffup/cli.py
+++ src/ffup/cli.py
@@ -4,6 +4,7 @@ import re
 import shutil
 import subprocess
 import sys
+import tempfile
 import zipfile
 from pathlib import Path
 
@@ -11,66 +12,65 @@ import fire
 import requests
 from tqdm import tqdm
 
-_TEMPFILE = Path('_ff.zip')
+_TMPDIR = tempfile.TemporaryDirectory()
 
 
 class FFUp:
 
     def __init__(self, sys=None, arch=None, repo=None, bin=None):
-        self.sys = sys or os.getenv('SYS') \
+        self.sys = sys or os.getenv('FF_SYS') \
             or platform.system().replace('Darwin', 'macOS').lower()
 
-        self.arch = arch or os.getenv('ARCH') \
+        self.arch = arch or os.getenv('FF_ARCH') \
             or ('arm64' if  platform.machine() in ['arm64', 'aarch64'] else 'amd64')
 
-        self.repo = repo or os.getenv('REPO') or 'snapshot'
-        self.bin = bin or os.getenv('BIN') or 'ffmpeg'
+        self.repo = repo or os.getenv('FF_REPO') or 'snapshot'
+        self.bin = bin or os.getenv('FF_BIN') or 'ffmpeg'
 
         self.URL = f'https://ffmpeg.martin-riedl.de/redirect/latest/{self.sys}/{self.arch}/{self.repo}/{self.bin}.zip'
 
-    def update(self, path=None, dry_run=False):
-        if path is None:
-            bin = shutil.which(self.bin)
-            if bin is None:
-                print('Error: failed to locate the installation.')
-                sys.exit(1)
-            else:
-                path = Path(bin).parent
-        else:
-            path = Path(path).expanduser()
-
-        self._current(path/self.bin)
-        self._latest()
-        if self.current_version!=self.latest_version:
-            if not dry_run:
-                self._download()
-                self._install(path)
-        else:
-            print('Already up to date.')
-
-    def install(self, path='~/.local/bin'):
-        path = Path(path).expanduser()
+    def install(self, dir=None):
+        path = Path(dir or os.getenv('XDG_BIN_HOME') or os.path.expanduser('~/.local/bin'), self.bin)
 
         if shutil.which(self.bin) is not None:
             print('Warning: found an existing installation on the `PATH`.')
 
-        if (path/self.bin).exists():
+        if path.exists():
             print('Error: found an existing installation at the given path.')
             sys.exit(1)
 
         self._latest()
-        self._download()
-        self._install(path)
+        file = self._download()
+        self._install(file, path)
 
-    def _current(self, bin):
-        if not bin.exists():
-            print('Error: no installation found.')
-            sys.exit(1)
+    def update(self, dir=None, dry_run=False):
+        if dir is None:
+            path = shutil.which(self.bin)
+            if path is None:
+                print('Error: no installation found on the `PATH`.')
+                sys.exit(1)
+        else:
+            path = Path(dir, self.bin)
+            if not path.exists():
+                print('Error: no installation found at the given path.')
+                sys.exit(1)
 
-        result = subprocess.check_output([bin, '-version'], text=True)
-        match = re.search(r'version (N-\d+-\w+|\d\.\d)', result)
+        self._current(path)
+        self._latest()
+        if self.current_version!=self.latest_version:
+            print('Update available.')
+            if not dry_run:
+                file = self._download()
+                self._install(file, path)
+        else:
+            print('Already up to date.')
+
+    def _current(self, path):
+        output = subprocess.check_output([path, '-version'], text=True)
+
+        match = re.search(r'version (N-\d+-\w+|\d\.\d)', output)
         if match is None:
-            print(f'Error: failed to parse current version from `{bin} -version` output.')
+            print(f'Error: failed to parse current version from `{path} -version` output.')
             sys.exit(1)
 
         self.current_version = match.group(1)
@@ -81,14 +81,14 @@ class FFUp:
         response.raise_for_status()
 
         if response.status_code==307:
-            redirect_url = response.headers['location']
-            match = re.search(r'_(N-\d+-\w+|\d\.\d)', redirect_url)
+            match = re.search(r'_(N-\d+-\w+|\d\.\d)', response.headers['location'])
             if match is None:
                 print('Error: failed to parse latest version from redirected url.')
                 sys.exit(1)
 
             self.latest_version = match.group(1)
             print('Latest version:', self.latest_version)
+
         else:
             print('Error: unexpected', response)
             print('Headers:', response.headers)
@@ -102,25 +102,28 @@ class FFUp:
                 unit='B', unit_scale=True,
                 desc='Downloading', dynamic_ncols=True
             )
-            with open(_TEMPFILE, 'wb') as zf:
+            file = Path(_TMPDIR.name, 'ff.zip')
+            with file.open('wb') as zf:
                 for chunk in response.iter_content(chunk_size=4096):
                     chunk_size = zf.write(chunk)
                     bar.update(chunk_size)
+        return file
 
-    def _install(self, path):
-        with zipfile.ZipFile(_TEMPFILE, 'r') as zf:
-            bin = zf.extract(self.bin)
+    def _install(self, file, path):
+        with zipfile.ZipFile(file, 'r') as zf:
+            bin = zf.extract(self.bin, _TMPDIR.name)
             os.chmod(bin, 0o755)
-            try:
-                shutil.move(bin, path)
-            except PermissionError:
-                subprocess.run(['sudo', 'mv', bin, path])
 
-        print('Successfully installed to:', path)
+        try:
+            os.replace(bin, path)
+        except PermissionError:
+            subprocess.run(['sudo', 'mv', bin, path], check=True, capture_output=True)
+
+        print('Successfully installed:', path)
 
 
 def main():
     try:
         fire.Fire(FFUp)
     finally:
-        _TEMPFILE.unlink(missing_ok=True)
+        _TMPDIR.cleanup()
